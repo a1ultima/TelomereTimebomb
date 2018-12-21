fb7725fb3322d3c148d047dba007f1503e77fc34
root <root@localhost.localdomain>
2018-12-21 12:01:24 -0500

    mathematical models for alternative assumptions and their derivations added to the 'derivations' directory

---
+++ derivations/python/punnet_to_collapseGametes.py
@@ -0,0 +1,10 @@
+
+
+import numpy as np
+
+
+#np.genfromtxt("C:\Users\Andrew Brockman\Dropbox\1_P2_hegTelomereTimebomb\! DRAFT\2014\Michael maths _2015-04-28\R\punnet.csv",delimiter=",")
+
+
+
+np.loadtxt(open("C:\Users\Andrew Brockman\Dropbox\1_P2_hegTelomereTimebomb\! DRAFT\2014\Michael maths _2015-04-28\R\punnet.csv","rb"),delimiter=",")
